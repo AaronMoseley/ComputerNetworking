728fdde21d6b5c2c2be0154cd56ef5829d6b121d
Aaron Moseley <36460809+AaronMoseley@users.noreply.github.com>
2022-11-13 15:32:32 -0500

    Add files via upload

--- FinalProject/Client1/TestClient.py
+++ FinalProject/Client1/TestClient.py
@@ -1,39 +1,56 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from threading import Thread
+from pathlib import Path
 import os
 import sys
 
 def main(hostname: str, portno: int)->None:
-    fileNames = ["test2.txt", "test.txt"]
-    
     conn = socket(AF_INET, SOCK_STREAM)
     conn.connect((hostname, portno))
 
-    for file in fileNames:
-        message = "REQ " + file
+    print("Connected to server")
+
+    fileName = input("Please input the name of the file you would like to request (or \"quit\" to exit): ")
+
+    while fileName.lower() != "quit":
+        message = "REQ " + fileName
         conn.sendall(message.encode('utf-8'))
 
-        f = open(os.path.join(sys.path[0], file), "wb")
+        if Path(os.path.join(sys.path[0], fileName)).is_file():
+            print(f"{fileName} already exists")
+            fileName = input("Please input the name of the file you would like to request (or \"quit\" to exit): ")
+            continue
+
+        print(f"Requested {fileName}")
+
+        f = open(os.path.join(sys.path[0], fileName), "wb")
 
         contents = conn.recv(1024)
         f.write(contents)
-        #while contents.decode('utf-8') != "EOF":
-        while sys.getsizeof(contents) >= 1024 and contents.decode("utf-8") != "EOF":
-            print(contents.decode('utf-8'))
-            
+
+        while sys.getsizeof(contents) >= 1024:
             contents = conn.recv(1024)
             f.write(contents)
 
         f.close()
 
-        print("sending ack")
+        print(f"Received {fileName}")
 
-        conn.sendall(("ACK " + file).encode('utf-8'))
+        conn.sendall(("ACK " + fileName).encode('utf-8'))
+
+        print(f"Acknowledged {fileName}")
+
+        fileName = input("Please input the name of the file you would like to request (or \"quit\" to exit): ")
 
     message = "END"
     conn.sendall(message.encode('utf-8'))
 
     conn.close()
 
+    print("Exiting")
+
 if __name__ == "__main__":
-    main("localhost", 6060)
+    ip = input("Please input the IP address of the server: ")
+    port = int(input("Please input the port to connect: "))
+
+    main(ip, port)
